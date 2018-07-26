7fafdf98652beb1db2684b35a8a7f14c401ea50a
Yotsakon <iyottt@gmail.com>
2018-07-27 01:46:45 +0700

    add all files

---
+++ layouts/default.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <section class="hero is-primary has-text-centered">
+      <div class="hero-body">
+        <div class="container">
+          <h1 class="title">
+            <nuxt-link to="/">Devda</nuxt-link>
+          </h1>
+          <h2 class="subtitle">
+            blog blog blog
+          </h2>
+        </div>
+      </div>
+    </section>
+    <nuxt/>
+  </div>
+</template>
+
+<style>
+html
+{
+  font-family: "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
+}
+</style>
---
+++ nuxt.config.js
@@ -0,0 +1,100 @@
+require('dotenv').config()
+const pkg = require('./package')
+const client = require('./plugins/contentful')
+
+module.exports = {
+  mode: 'universal',
+
+  /*
+  ** Headers of the page
+  */
+  head: {
+    title: pkg.name,
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: pkg.description }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+
+  /*
+  ** Customize the progress-bar color
+  */
+  loading: { color: '#FFFFFF' },
+
+  /*
+  ** Global CSS
+  */
+  css: [
+  ],
+
+  /*
+  ** Plugins to load before mounting the App
+  */
+  plugins: [
+    '@/plugins/contentful'
+  ],
+
+  /*
+  ** Nuxt.js modules
+  */
+  modules: [
+    '@nuxtjs/bulma',
+    '@nuxtjs/dotenv',
+    '@nuxtjs/markdownit',
+  ],
+  markdownit: {
+    injected: true
+  },
+  /*
+  ** Axios module configuration
+  */
+  axios: {
+    // See https://github.com/nuxt-community/axios-module#options
+  },
+
+  /*
+  ** Build configuration
+  */
+  build: {
+    postcss: {
+      plugins: {
+        'postcss-cssnext': {
+          features: {
+            customProperties: false
+          }
+        }
+      }
+    },
+    /*
+    ** You can extend webpack config here
+    */
+    extend(config, ctx) {
+      // Run ESLint on save
+      if (ctx.isDev && ctx.isClient) {
+        config.module.rules.push({
+          enforce: 'pre',
+          test: /\.(js|vue)$/,
+          loader: 'eslint-loader',
+          exclude: /(node_modules)/
+        })
+      }
+    }
+  },
+  generate: {
+    routes() {
+      return client.getEntries({'content_type': 'post'})
+        .then(entries => {
+          return entries.items.map(entry => {
+            return {
+              route: entry.fields.slug,
+              payload: entry
+            }
+          })
+        })
+    }
+  }
+}
