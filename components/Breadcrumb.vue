fdf5820180f4e6b6dece81653da64516d33183c2
Yotsakon <iyottt@gmail.com>
2018-08-03 15:33:43 +0700

    update

---
+++ components/Breadcrumb.vue
@@ -0,0 +1,14 @@
+<template>
+    <nav class="breadcrumb is-centered" aria-label="breadcrumbs">
+        <ul>
+            <li><nuxt-link to="/">Home</nuxt-link></li>
+            <li class="is-active"><a href="#" aria-current="page">{{ this.$route.params.slug }}</a></li>
+        </ul>
+    </nav>
+</template>
+
+<script>
+export default {
+}
+</script>
+
